92e047462290ed87e810d1ecbf77054b4f20655b
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-05-20 10:59:36 -0300

    CRUD service with mocked data

--- README.md
+++ README.md
@@ -46,6 +46,20 @@ $ nestjs new
 
 ```bash
 nest generate controller
+nest g co
 nest generate controller modules/abc
 nest generate controller modules/abc --dry-run
 ```
+
+**Generating a service**
+
+```bash
+nest generate service
+nest g s
+nest generate service modules/abc
+nest generate service modules/abc --dry-run
+```
+
+In NestJS, each service is a provider. It's just a class annoted with the decorator called @Injectable.
+
+Provider, it can inject dependencies so NestJS system will handle all of that instead of the developer having to do it manually
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,44 +8,35 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
-  // Loose compatibility with Nest features that depend on Nest standard response handling, which are Interceptors and HttpCode decorator
-  // It becomes plataform dependent and harder to test (mock response object)
-  // It is possible to specify the name of the nested route by placing a string inside the decorator
-  // @Get('flavors')
-  // findAll(@Res() response) {
-  //   response.status(200).send('This action returns all coffees');
-  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
-    // Instead of receiving all params at the decorator, we can specify exactly which ones do we want
-    // findOne(@Param() params) {
-    // return `This action returns #${params.id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
-    // We can also choose to receive only a specific property from the body, which is not a good practice because you might miss some validations
-    // create(@Body('name') body) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
