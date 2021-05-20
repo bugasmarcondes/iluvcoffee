4f1c623f1ab0433101061600981236a8a56b7f05
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-05-20 10:15:15 -0300

    Update and remove routes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -27,5 +35,15 @@ export class CoffeesController {
     return body;
     // We can also choose to receive only a specific property from the body, which is not a good practice because you might miss some validations
     // create(@Body('name') body) {
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
