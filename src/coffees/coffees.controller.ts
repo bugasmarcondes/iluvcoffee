a6d66179c34facda99560c0a149921d7d8175737
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-05-20 09:52:11 -0300

    Using route parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+    // Instead of receiving all params at the decorator, we can specify exactly which ones do we want
+    // findOne(@Param() params) {
+    // return `This action returns #${params.id} coffee`;
   }
 }
