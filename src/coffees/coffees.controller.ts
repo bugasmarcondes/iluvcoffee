74de6fdc7a6b6309ab2f639b592ce88e4be8719d
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-05-25 14:46:26 -0300

    Using tags to group resources

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,7 +10,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
-import { ApiForbiddenResponse } from '@nestjs/swagger';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
 import { Public } from 'src/common/decorators/public.decorator';
@@ -20,6 +20,7 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(
